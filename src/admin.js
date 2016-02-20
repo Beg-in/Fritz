8d8cfb567319150bca584423bbf9ec6f98d3023e
snatera15 <snatera09@gmail.com>
2016-02-20 17:31:53 -0600

    add route for model

--- src/admin.js
+++ src/admin.js
@@ -4,6 +4,11 @@ module.exports = function(path, app, express, config) {
 
     app.use('/fritz', express.static(path.join(config.paths.fritz, 'admin')));
 
+
+    app.use('/fritz/models', function() {
+    	
+    });
+
     return {
         tables: {}
     };
