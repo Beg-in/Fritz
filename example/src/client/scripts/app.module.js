c6d9975a347102edeecde26e2015e8fd669a05b5
Cody Sherman <meatwaddancin@gmail.com>
2016-02-20 16:20:35 -0600

    Sass rules added

--- example/src/client/scripts/app.module.js
+++ example/src/client/scripts/app.module.js
@@ -37,24 +37,14 @@ angular.module('exampleApp', [
     $location
 ) { 'use strict';
 
-    // $rootScope.$on('$routeChangeStart', function(event, next) {
-    //     $window.scrollTo(0,0);
-    //     var c = next.$$route.controller ? next.$$route.controller.indexOf('Controller') : -1;
-    //     if(c === -1) {
-    //         $rootScope.pageName = '';
-    //         return;
-    //     }
-    //     $rootScope.pageName = next.$$route.controller.substring(0, c) + 'Page';
-    // });
-    //
-    // $rootScope.$on('$routeChangeSuccess', function() {
-    //     if ($rootScope.pageName === 'homePage') {
-    //         $rootScope.noBootstrap = true;
-    //         $rootScope.firstLoad = true;
-    //     } else {
-    //         $rootScope.noBootstrap = false;
-    //         $rootScope.firstLoad = true;
-    //     }
-    // });
+    $rootScope.$on('$routeChangeStart', function(event, next) {
+        $window.scrollTo(0,0);
+        var c = next.$$route.controller ? next.$$route.controller.indexOf('Controller') : -1;
+        if(c === -1) {
+            $rootScope.pageName = '';
+            return;
+        }
+        $rootScope.pageName = next.$$route.controller.substring(0, c) + 'Page';
+    });
 
 });
