b2e63c862ac35d5c540593ad9c70b81816f563b1
David Song <david.song0802@gmail.com>
2016-02-21 02:00:38 -0500

    added editable directives

---
+++ admin/scripts/directives/editable.directive.js
@@ -0,0 +1,40 @@
+angular.module('bnMentorsApp').directive('editable', function(
+    $rootScope,
+    $location,
+    $window,
+    $compile
+) { 'use strict';
+
+    return {
+      restrict: 'E',
+      transclude: true,
+      templateUrl: '/views/editable.html',
+      // scope: {
+      //   editableName: '=name'
+      // },
+      link: function ($scope, $ielem, $iattrs) {
+        $scope.editableName = $iattrs.name;
+
+        var html = $ielem.html();
+
+        $scope.beginEdit = function (name) {
+          document.querySelector("editable[name='" + name + "']").contentEditable = "true";
+        };
+
+        $scope.cancelEdit = function (name) {
+          document.querySelector("editable[name='" + name + "']").contentEditable = "false";
+        };
+
+        $scope.saveEdit = function (name) {
+          $scope.cancelEdit(name);
+          var newHTML = document.querySelector("editable[name='" + name + "'] .editable-inner-wrap").innerHTML;
+          console.log(newHTML);
+        };
+
+        // html.replace(/\{\{(.*?)\}\}/, "DYNAMIC CONTENT!");
+        // var e = $compile(html)($scope);
+        // $ielem.replaceWith(e);
+      }
+    };
+
+});
